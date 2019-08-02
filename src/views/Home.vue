6a0564b016b08974bc5ed235fe768a5b1143c853
Kostya <kimanezhin@edu.hse.ru>
2019-08-02 15:08:51 +0300

    right menu style bug fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div>
     <Header />
     <div id="main">
-      <Main @listSizeChanged="onListSizeChanged"/>
-      <FilterMenu v-if="isNotEmpty"/>
+      <Main @listSizeChanged="onListSizeChanged" />
+      <FilterMenu class="filterMenu" />
     </div>
   </div>
 </template>
@@ -12,7 +12,7 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Axios from "axios";
-import FilterMenu from '../components/FilterMenu'
+import FilterMenu from "../components/FilterMenu";
 export default {
   components: {
     Header,
@@ -21,24 +21,31 @@ export default {
   },
   data() {
     return {
-      isNotEmpty:false
-    }
+      rightMenu:''
+    };
   },
   methods: {
-    onListSizeChanged(size){
-      this.isNotEmpty = !!size;
+    onListSizeChanged(size) {
+      if (!size)
+        this.rightMenu.style.display = "none";
+      else
+        this.rightMenu.style.display =
+          "initial";
     }
+  },
+  mounted() {
+    this.rightMenu = document.getElementsByClassName("filterMenu")[0];
   },
 };
 </script>
 <style scoped>
-#main{
+#main {
   display: flex;
 }
 
 @media screen and (max-width: 830px) {
- #main{
-   flex-direction: column-reverse;
- }   
+  #main {
+    flex-direction: column-reverse;
+  }
 }
 </style>
